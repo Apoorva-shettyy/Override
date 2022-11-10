a89616bea583d4bd3e88009dcb034368a790f70b
Apoorva <apoorvaks2022@gmail.com>
2022-11-10 18:35:28 +0530

    Add project files.

---
+++ Override/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+namespace Program
+{
+    public class Parent
+    {
+        public virtual void place()
+        {
+            Console.WriteLine("KAASHI");
+        }
+        public virtual void travell()
+        {
+            Console.WriteLine("BUS");
+        }
+    }
+    public class Child : Parent
+    {
+
+        public override void place()
+        {
+            Console.WriteLine("GOA");
+        }
+        public override void travell()
+        {
+            Console.WriteLine("TRAIN");
+        }
+    }
+    class main
+    {
+        public static void Main()
+        {
+            //Parent p = new Parent();
+            Parent c = new Child();
+
+
+            c.travell();
+            c.place();
+        }
+
+    }
+}
